e7678df731268fd3c9f22f24ed20311d205425fe
ARCHITECT25 <spartancain@aol.com>
2025-03-17 00:13:51 -0400

    17MAR25

--- app/src/main/java/com/example/sqlsandbox03/IncidentModel.kt
+++ app/src/main/java/com/example/sqlsandbox03/IncidentModel.kt
@@ -2,11 +2,14 @@ package com.example.sqlsandbox03
 
 import java.sql.Date
 
-data class Incident {
-    var id: Int,
+//no idea if I'm building this right... data or constructor required??
+data class Incident public constructor(
+    val IncidentID: Int,
     var IncidentName: String,
     var IncidentType: String,
     var IncidentLoc: String,
-    lateinit var IncidentStart: Date,
-    lateinit var IncidentEnd: Date
+    val IncidentStart: Date,
+    var IncidentEnd: Date
+) {
+
 }
